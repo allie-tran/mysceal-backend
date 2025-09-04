ffb7038218572f08fca2f44e3da172068c23aa94
Allie Tran <ld.allie.tran@gmail.com>
2025-09-04 16:33:49 +0100

    ♻️ Clean up

--- project/install.sh
+++
@@ -1,9 +0,0 @@
-set -e
-
-conda env create -f environment.yml
-
-pip install auto-gptq --extra-index-url https://huggingface.github.io/autogptq-index/whl/cu118/ --no-cache-dir --use-deprecated=legacy-resolver
-
-
-
-
